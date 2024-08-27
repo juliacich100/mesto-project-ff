2829327e40135b4ed631512da11beabc834c0b85
Iuliia Balashova <Iuliia-14@yandex.ru>
2024-08-27 14:38:11 +0300

    fix: reviewer's remarks

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      desc: "покрытые зеленью горы Архыза"
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      desc: "река, берега которой покрыты легким слоем снега"
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      desc: "серые бетонные многоэтажки"
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      desc: "рельеф Камчатки: низменность на переднем фоне, переходящая в горы на заднем фоне"
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      desc: "железная дорога, проходящая через лес"
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      desc: "заснеженные скалистые берега Байкала"
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -7,28 +7,27 @@ const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function addCard() {
-    initialCards.forEach(function(place) {
-        const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+function createCard(cardData) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
 
-        cardElement.querySelector('.card__image').src = place.link;
-        cardElement.querySelector('.card__title').textContent = place.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.desc;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
 
-        placesList.append(cardElement);
+        cardElement.querySelector('.card__delete-button').addEventListener('click', () => removeCard(cardElement));
 
-        cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
-    })
+    return cardElement;
 }
-
 
 // @todo: Функция удаления карточки
 
-function removeCard(button) {
-    const targetButton = button.target;
-    const buttonCard = targetButton.closest('.places__item'); 
-    buttonCard.remove();
+function removeCard(cardElement) {
+    cardElement.remove();
 }
 
 // @todo: Вывести карточки на страницу
 
-addCard();
+initialCards.forEach(item => {
+    placesList.append(createCard(item));
+})
